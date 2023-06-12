4e1d2e03c9f98b0255276a2b04cbf9bebadc90d7
Romchig <romulusto@gmail.com>
2023-06-12 21:17:53 +0200

    9 из 10 сбой

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,45 @@
 public class Main {
     public static void main(String[] args) {
 
+        for (int i = 1; i <= 10; i++) {System.out.println(i);}
+
+        //Задание номер 2
+        for (int e = 10; e >= 1; e = e - 1) {System.out.println(e);}
+
+        //Задание номер 3
+        for (int w = 0; w <= 17; w++) {
+            if (w % 2 == 0) {System.out.println(w);}
+        }
+
+        //Задание номер 3 версия 2
+        for (int q = 0; q <= 17; q = q + 2) {System.out.println(q);}
+
+        //Задание номер 4
+        for (int r = 10; r >= -10; r = r - 1) {System.out.println(r);}
+
+        //Задание номер 5
+        for (int t = 1904; t <= 2096; t = t + 4) {System.out.println(t + " Год является високосным");}
+
+        //Задание номер 6
+        for (int с = 7; с <= 98; с = с + 7) {System.out.println(с);}
+
+        //Задание номер 7
+        for (int y = 1; y <= 512; y = y * 2) {System.out.println(y);}
+
+        //Задание номер 8
+        int savings = 29000;
+        int total = 0;
+        for (int m = 0; m <= 12; m++) {
+            total = total + savings;
+            System.out.println(total);}
+
+        //Задание номер 9
+        int savings1 = 29000;
+        int total1 = 0;
+        for (int m1 = 1; m1 <= 12; m1++) {
+            total1 = total1 + total1/100;
+            total1 = total1 + savings1;
+            System.out.println("Месяц " + m1 + ", cумма накоплений равна " + total1+ " рублей" );}
+
     }
 }
